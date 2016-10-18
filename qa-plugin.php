f9a334541ac581a4b7c0925e44a496bfcb2aaf31
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-10-18 17:54:04 +0900

    Add plugin file

---
+++ qa-plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+    Plugin Name: Custom Related Questions
+    Plugin URI:
+    Plugin Description: Customize related questions widget
+    Plugin Version: 1.0
+    Plugin Date: 2016-10-18
+    Plugin Author: 38qa.net
+    Plugin Author URI: http://38qa.net/
+    Plugin License: GPLv2
+    Plugin Minimum Question2Answer Version: 1.7
+    Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+    header('Location: ../../');
+    exit;
+}
