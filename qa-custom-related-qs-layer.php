dc998c3bec9a1a7737085c2acb2448c5bad2ef35
yshiga <yuichi.shiga@gmail.com>
2017-06-20 16:44:21 +0900

    delete infinite scroll on question detail page

--- qa-custom-related-qs-layer.php
+++ qa-custom-related-qs-layer.php
@@ -6,47 +6,20 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 
 class qa_html_theme_layer extends qa_html_theme_base
 {
-    private $infscr = null;
     private $pluginurl = '';
-    
+
     function qa_html_theme_layer($template, $content, $rooturl, $request)
     {
         qa_html_theme_base::qa_html_theme_base($template, $content, $rooturl, $request);
-        if(infinite_scroll_available()) {
-            require_once QA_PLUGIN_DIR . 'q2a-infinite-scroll/qa-infinite-scroll.php';
-            $this->infscr = new qa_infinite_scroll();
-        }
         $this->pluginurl = qa_opt('site_url').'qa-plugin/q2a-custom-related-questions/';
     }
 
     function head_script()
     {
         qa_html_theme_base::head_script();
-        if ($this->template === 'question' && isset($this->infscr)) {
-            if (strpos(qa_opt('site_theme'), 'q2a-material-lite') !== false) {
-                $this->output('<script>var material_lite = true;</script>');
-            } else {
-                $this->output('<script>var material_lite = false;</script>');
-            }
-            $lang_json = json_encode (array(
-              'read_next' => qa_lang_html('custom_related_qs/read_next'),
-              'read_previous' => qa_lang_html('custom_related_qs/read_previous'),
-            ));
-            $this->output(
-              '<script>',
-              "var crq_lang = $lang_json ;",
-              '</script>'
-            );
-            $this->output('<SCRIPT TYPE="text/javascript" SRC="'.$this->infscr->pluginjsurl.'jquery-ias.min.js"></SCRIPT>');
-            $this->output('<SCRIPT TYPE="text/javascript" SRC="'.$this->pluginurl.'js/infinite-scroll-recent.js"></SCRIPT>');
-            $this->output('<SCRIPT TYPE="text/javascript" SRC="'.$this->pluginurl.'js/optimizely.js"></SCRIPT>');
-        }
     }
     function head_css()
     {
         qa_html_theme_base::head_css();
-        if ($this->template === 'question' && isset($this->infscr)) {
-            $this->output('<LINK REL="stylesheet" TYPE="text/css" HREF="'.$this->infscr->plugincssurl.'jquery.ias.css"/>');
-        }
     }
 }
